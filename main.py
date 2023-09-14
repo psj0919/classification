8f8345ef5110b8dbbe71aa54f2e57a434e609451
psj0919 <darkjyuk@gmail.com>
2023-09-14 22:58:09 +0900

    .

--- main.py
+++ main.py
@@ -16,14 +16,31 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+def eval():
+
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
 def train():
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(1):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+            #inputs, labels = data[0].to(device), data[1].to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -47,20 +64,50 @@ def train():
 if __name__ =='__main__':
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # --batch_size--
+    batch_size = 1
+    # --------------
+    # -----------------------------setup device----------------------------
+    # gpu_id = '1'
+    # device = torch.device("cuda:{}".format(gpu_id) if torch.cuda.is_available() else "cpu")
+    # ---------------------------------------------------------------------
 
-    batch_size = 4
-
+    #-------------------------------------dataset_download-------------------------------------------------
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    # ------------------------------------------------------------------------------------------------------
 
+    # ----------------------------------------classes-----------------------------------------
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # -----------------------------------------------------------------------------------------
 
+    # --network_setup--
     net = Net()
+    # net.to(device)
+    # -----------------
 
+
+    # ------------loss, optimizer, scheduler_setup--------------------
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    #optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=0.0001, weight_decay =5e-5)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.1)
+    # -----------------------------------------------------------------
+
 
     train()
+    path = './checkpoints/cifar_net.pth'
+    torch.save(net.state_dict(), path)
+
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+
+    net = Net()
+    net.load_state_dict(torch.load(path))
+    eval()
+
+
+
 
